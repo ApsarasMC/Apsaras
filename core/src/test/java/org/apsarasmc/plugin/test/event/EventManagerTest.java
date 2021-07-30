6025046e139f32f48324d45930a358b537d786a6
InkerBot <2924657226@qq.com>
2021-07-30 22:33:10 +0800

    Bump depends version

--- core/src/test/java/org/apsarasmc/plugin/test/event/EventManagerTest.java
+++ core/src/test/java/org/apsarasmc/plugin/test/event/EventManagerTest.java
@@ -26,8 +26,6 @@ class EventManagerTest {
   @Test
   @Order (0)
   void register() {
-    eventManager.registerListeners(pluginContainer, this);
-
     List< org.apsarasmc.apsaras.event.Order > orderList = Arrays.asList(org.apsarasmc.apsaras.event.Order.values());
     Collections.shuffle(orderList);
     for (org.apsarasmc.apsaras.event.Order order : orderList) {
@@ -39,69 +37,6 @@ class EventManagerTest {
     Assertions.assertEquals(org.apsarasmc.apsaras.event.Order.POST.order() + 1, pos, "Listener called in wrong order.");
   }
 
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.PRE)
-  public void vb4(TestEvent event) {
-    if (pos == -4) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.AFTER_PRE)
-  public void vb3(TestEvent event) {
-    if (pos == -3) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.FIRST)
-  public void vb2(TestEvent event) {
-    if (pos == -2) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.EARLY)
-  public void vb1(TestEvent event) {
-    if (pos == -1) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.DEFAULT)
-  public void v0(TestEvent event) {
-    if (pos == 0) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.LATE)
-  public void va1(TestEvent event) {
-    if (pos == 1) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.LAST)
-  public void va2(TestEvent event) {
-    if (pos == 2) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.BEFORE_POST)
-  public void va3(TestEvent event) {
-    if (pos == 3) {
-      pos++;
-    }
-  }
-
-  @EventHandler (order = org.apsarasmc.apsaras.event.Order.POST)
-  public void va4(TestEvent event) {
-    if (pos == 4) {
-      pos++;
-    }
-  }
-
   public class TestEventListener implements EventListener< TestEvent > {
     public final org.apsarasmc.apsaras.event.Order order;
 
@@ -110,7 +45,7 @@ class EventManagerTest {
     }
 
     @Override
-    public void handle(final TestEvent event) throws Exception {
+    public void handle(final TestEvent event) {
       Assertions.assertEquals(order.order(), pos++);
     }
   }
